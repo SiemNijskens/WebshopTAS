96263bd2be6b021b14de6aa8d8db1a1dd66913b3
Stefan Kiers <stefan.kiers@itvitaelearning.nl>
2025-12-02 13:18:07 +0100

    Initial commit for setting up local repo

---
+++ backend/src/test/java/com/Webshop/ClassAssignment/ItVitae/Webshop/WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Webshop.ClassAssignment.ItVitae.Webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

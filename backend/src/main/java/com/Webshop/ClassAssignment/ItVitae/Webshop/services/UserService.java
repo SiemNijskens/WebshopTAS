3ea562c0aa541d8e9ca5f6eaff64c02e4181160b
ThomazSDA <thomas_wennink@hotmail.com>
2025-12-10 14:53:44 +0100

    Updated userservice

--- backend/src/main/java/com/Webshop/ClassAssignment/ItVitae/Webshop/services/UserService.java
+++ backend/src/main/java/com/Webshop/ClassAssignment/ItVitae/Webshop/services/UserService.java
@@ -3,6 +3,7 @@ package com.Webshop.ClassAssignment.ItVitae.Webshop.services;
 import com.Webshop.ClassAssignment.ItVitae.Webshop.dtos.user.UserCreateDTO;
 import com.Webshop.ClassAssignment.ItVitae.Webshop.dtos.user.UserDTO;
 import com.Webshop.ClassAssignment.ItVitae.Webshop.dtos.user.UserSummaryDTO;
+import com.Webshop.ClassAssignment.ItVitae.Webshop.dtos.user.UserUpdateDTO;
 import com.Webshop.ClassAssignment.ItVitae.Webshop.exceptions.EmailAlreadyInUseException;
 import com.Webshop.ClassAssignment.ItVitae.Webshop.exceptions.UserNotFoundException;
 import com.Webshop.ClassAssignment.ItVitae.Webshop.models.User;
@@ -50,5 +51,21 @@ public class UserService {
 //        user.setPassword(encodedPassword);
         User savedUser = userRepository.save(user);
         return UserDTO.fromEntity(savedUser);
+    }
+
+    public UserDTO updateUser(Long id, UserUpdateDTO userUpdateDTO) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with " + id + " not found"));
+
+        userUpdateDTO.updateEntity(user);
+        User savedUser = userRepository.save(user);
+        return UserDTO.fromEntity(savedUser);
+    }
+
+    public void deleteUser(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with " + id + " not found"));
+
+        userRepository.delete(user);
     }
 }

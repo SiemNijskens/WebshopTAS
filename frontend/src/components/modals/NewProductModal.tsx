ce755e45b773b1664fa38f09de7a2bade2948396
Siem Nijskens <siemnijskens@gmail.com>
2026-01-07 10:42:40 +0100

    new product modal

---
+++ frontend/src/components/modals/NewProductModal.tsx
@@ -0,0 +1,359 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useState } from "react"
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap"
+import type { productCreateDTO } from "../../types/form.types"
+
+interface variant {
+    key: number,
+    variantAttributeOne: variantAttribute,
+    variantAttributeTwo: variantAttribute,
+    stock: number,
+    price: number,
+    imageURL: string
+}
+
+interface variantAttribute {
+    attribute: string,
+    value: string | undefined
+}
+
+const NewProductModal = () => {
+
+    const [formData, setFromData] = useState({
+        productDefaultImageURL: '',
+        productName: '',
+        productBrand: '',
+        productDescription: '',
+        productType: '',
+        uniqueAttributes: {},
+        variants: {}
+    })
+
+    const [variants, setVariants] = useState<variant[]>([])
+
+    const [variantOneData, setVariantOneData] = useState({
+        attributeOne: "",
+        valuesOne: ['']
+    })
+
+    const [variantTwoData, setVariantTwoData] = useState({
+        attributeTwo: "",
+        valuesTwo: ['']
+    })
+
+    const [numberOfVariants, setNumberOfVariants] = useState(1)
+    const [numberOfUniqueAttributes, setNumberOfUniqueAttributes] = useState([1])
+
+    // const [AttributeOnedisabled, setAttributeOneDisabled] = useState(false)
+    // const [AttributeTwoDisabled, setAttributeTwoDisabled] = useState(false)
+
+    const [variantAttributeOne, setVariantAttributeOne] = useState([0]);
+    const [variantAttributeTwo, setVariantAttributeTwo] = useState([0]);
+
+    const queryClient = useQueryClient();
+
+    const createProduct = useMutation({
+        mutationFn: async (FormData: productCreateDTO) => {
+            const response = await fetch(`${API_URL}/product`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(FormData)
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to create product')
+            };
+
+            return response.json();
+        },
+
+        onSuccess: (data) => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            queryClient.invalidateQueries({ queryKey: ['products'] }),
+                console.log('products created successfully', data),
+                setFromData({
+                    productDefaultImageURL: '',
+                    productName: '',
+                    productBrand: '',
+                    productDescription: '',
+                    productType: '',
+                    uniqueAttributes: {},
+                    variants: {}
+                });
+        }
+    })
+
+    const addVariantAttributeOne = () => {
+        setVariantAttributeOne([...variantAttributeOne, variantAttributeOne.length])
+    }
+
+    const addVariantAttributeTwo = () => {
+        setVariantAttributeTwo([...variantAttributeTwo, variantAttributeTwo.length])
+    }
+
+    const removeVariantAttributeOne = () => {
+        const length = variantAttributeOne.length
+        const updatedList = variantAttributeOne.slice(0, (length - 1))
+        setVariantAttributeOne(updatedList);
+    }
+
+    const removeVariantAttributeTwo = () => {
+        const length = variantAttributeTwo.length
+        const updatedList = variantAttributeTwo.slice(0, (length - 1))
+        setVariantAttributeTwo(updatedList);
+    }
+
+    const addUniqueAttribute = () => {
+        setNumberOfUniqueAttributes([...numberOfUniqueAttributes, numberOfUniqueAttributes.length + 1])
+    }
+
+    const removeUniqueAttribute = () => {
+        const length = numberOfUniqueAttributes.length
+        const updatedLength = numberOfUniqueAttributes.slice(0, (length - 1))
+        setNumberOfUniqueAttributes(updatedLength)
+    }
+
+    // console.log("number of unique attributes", numberOfUniqueAttributes)
+
+    const addAttributeOne = (eventAttributeOne) => {
+        eventAttributeOne.preventDefault()
+        // setAttributeOneDisabled(true)
+        const attributeOneData = new FormData(eventAttributeOne.target);
+        const data = Object.fromEntries(attributeOneData.entries());
+        const attributeOne = data.attribute as string
+        delete data.attribute;
+        const valuesAttributeOne = Object.values(data)
+
+        setVariantOneData({ attributeOne: attributeOne, valuesOne: valuesAttributeOne })
+    }
+
+
+    const addAttributeTwo = (eventAttributeTwo) => {
+        eventAttributeTwo.preventDefault()
+        const attributeTwoDataRaw = new FormData(eventAttributeTwo.target);
+        const attributeTwodata = Object.fromEntries(attributeTwoDataRaw.entries());
+        const attributeTwo = attributeTwodata.attribute as string
+        delete attributeTwodata.attribute;
+        const valuesAttributeTwo = Object.values(attributeTwodata)
+
+        setVariantTwoData({ attributeTwo: attributeTwo, valuesTwo: valuesAttributeTwo })
+    }
+
+    const changeNumberOfVariants = (eventNumberOfVariants: { target: { value: string } }) => {
+        setNumberOfVariants(parseInt(eventNumberOfVariants.target.value))
+    }
+
+    let variantTwoHidden = true
+    if (numberOfVariants === 2) { variantTwoHidden = false }
+    else variantTwoHidden = true
+
+    const updateVariantTable = () => {
+        const variantsArray: variant[] = [];
+        let number = 0
+        for (let i = 0; i < variantOneData.valuesOne.length; i++) {
+            for (let j = 0; j < variantTwoData.valuesTwo.length; j++) {
+                variantsArray.push({ key: number, variantAttributeOne: { attribute: variantOneData.attributeOne, value: variantOneData.valuesOne.at(i) }, variantAttributeTwo: { attribute: variantTwoData.attributeTwo, value: variantTwoData.valuesTwo.at(j) }, stock: NaN, price: NaN, imageURL: '' })
+                number = number + 1
+            }
+        }
+        setVariants(variantsArray)
+        console.log(variants[1])
+    }
+
+    const handleChange = (key: number, event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        const updatedVariants: variant[] = [...variants];
+        updatedVariants[key] = { ...variants[key], [name]: value }
+        setVariants(updatedVariants);
+    }
+    // console.log(variants)
+
+    const handleChangeBaseProduct = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        setFromData({ ...formData, [name]: value })
+    }
+
+    // (6)['material', 'cotton', 'neck type', 'v neck', 'attriubute 3', 'value 3']
+
+    const lockInUniqueAttributes = (lockInEvent) => {
+        lockInEvent.preventDefault()
+        const uniqueAttributesData = new FormData(lockInEvent.target)
+        const data = Object.fromEntries(uniqueAttributesData.entries())
+        const uniqueAttributesObject = {};
+        const values = Object.values(data);
+
+        for (let i = 0; i < numberOfUniqueAttributes.length; i++) {
+            const key = values[i * 2]
+            uniqueAttributesObject[key] = values[(i * 2 + 1)]
+        }
+        console.log(uniqueAttributesObject)
+        setFromData({ ...formData, uniqueAttributes: uniqueAttributesObject })
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        createProduct.mutate(formData);
+    }
+
+    //   console.log(formData.productName)
+    //   console.log(formData.productBrand)
+    //   console.log(formData.productDescription)
+    //   console.log(formData.productType)
+    //   console.log(formData.productDefaultImageURL)
+
+    return (
+        <>
+            <Form>
+                <Card>
+                    <Form.Group className="mb-3">
+                        <Form.Label>product name</Form.Label>
+                        <Form.Control placeholder="basic t-shirt" value={formData.productName} type="text" id="productName" name="productName" onChange={() => handleChangeBaseProduct(event)} />
+
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>product brand</Form.Label>
+                        <Form.Control placeholder="nike" value={formData.productBrand} id="productBrand" name="productBrand" onChange={() => handleChangeBaseProduct(event)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>product description</Form.Label>
+                        <Form.Control as={"textarea"} rows={3} value={formData.productDescription} name="productDescription" placeholder="basic t-shirt" onChange={() => handleChangeBaseProduct(event)} />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>type product</Form.Label>
+                        <Form.Select aria-placeholder="type of product" value={formData.productType} name="productType" onChange={() => handleChangeBaseProduct(event)}>
+                            <option hidden value="">select a type</option>
+                            <option>top</option>
+                            <option>bottom</option>
+                            <option>shoes</option>
+                            <option>accessories</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>base product image URL</Form.Label>
+                        <Form.Control placeholder="URL" value={formData.productDefaultImageURL} name="productDefaultImageURL" onChange={() => handleChangeBaseProduct(event)} />
+                    </Form.Group>
+                    <br />
+                </Card>
+            </Form>
+
+            <Form onSubmit={lockInUniqueAttributes}>
+                <Card>
+                    <Form.Group className="mb-3" >
+                        <Form.Label>Unique attribute</Form.Label>
+                        {/* {console.log(numberOfUniqueAttributes)} */}
+                        {numberOfUniqueAttributes.map((index) => (
+                            <Row key={index}>
+                                <Col> Attribute:<Form.Control placeholder="Attribute (eg. material)" type="text" name={"attribute" + index} /></Col>
+                                <Col> Value:<Form.Control placeholder="Value (eg. cotton)" type="text" name={"value" + index} /> </Col>
+                                <Col> <br />
+                                </Col> </Row>
+                        ))}
+                        <Button variant="primary" onClick={addUniqueAttribute}>Add unique attribute</Button>
+                        <Button variant="primary" onClick={removeUniqueAttribute}>remove unique attribute</Button>
+                    </Form.Group>
+                    <br />
+                    <Button type="submit" variant="primary">lock in these unique attribute</Button>
+                </Card>
+            </Form>
+
+            select number of variant attributes
+            <br />
+            <select value={numberOfVariants} onChange={changeNumberOfVariants}>
+                <option value="1" >One</option>
+                <option value="2" >Two</option>
+            </select>
+            <Card>
+                <Container style={{ border: "1px, solid, light gray" }}>
+                    <Row style={{ display: "flex", justifyContent: "center" }}>
+                        <Col>
+                            <Form onSubmit={addAttributeOne}>
+                                <Form.Group className="mb-3">
+                                    <Col>
+                                        <Form.Label>Variant attribute one</Form.Label>
+                                        <Col>
+                                            <Form.Control placeholder="Attribute (eg. size)" type="text" name="attribute" /></Col>
+
+                                        <div>
+                                            Values
+                                            {variantAttributeOne.map((index) => (
+                                                <Col key={index}>
+                                                    <Form.Control placeholder="Value (eg. large)" type="text" name={"value" + index} />
+                                                </Col>
+                                            ))}
+                                        </div>
+                                        <Row>
+                                            <Col> <Button variant="primary" onClick={addVariantAttributeOne}> Add value </Button> </Col>
+                                            <Col> <Button variant="primary" onClick={removeVariantAttributeOne}> Delete value </Button> </Col>
+                                        </Row>
+
+                                        <br />
+
+                                        <Button type="submit" variant="primary"> add this variant attribute </Button>
+
+                                    </Col>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                        <Col hidden={variantTwoHidden}>
+                            <Form onSubmit={addAttributeTwo}>
+                                <Form.Group className="mb-3">
+                                    <Form.Label >Variant attribute two</Form.Label>
+                                    <Col>
+                                        <Form.Control placeholder="Attribute (eg. color)" type="text" name="attribute" />
+                                    </Col>
+
+                                    <div>
+                                        Values
+                                        {variantAttributeTwo.map((index) => (
+                                            <Col key={index}>
+                                                <Form.Control placeholder="Value (eg. blue)" type="text" name={"value" + index} />
+                                            </Col>
+                                        ))}
+                                    </div>
+                                    <Row>
+                                        <Col> <Button variant="primary" onClick={addVariantAttributeTwo}> Add value </Button> </Col>
+                                        <Col> <Button variant="primary" onClick={removeVariantAttributeTwo}> Delete value </Button> </Col>
+                                    </Row>
+
+                                    <br />
+
+                                    <Button type="submit" variant="primary"> add this variant attribute </Button>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </Card >
+
+            <Button type="button" variant="primary" onClick={updateVariantTable}> update table </Button>
+
+
+            {variants.map((variant) => (
+                <div key={variant.key}>
+                    {variant.variantAttributeOne.attribute}: {variant.variantAttributeOne.value}
+                    {variant.variantAttributeTwo.attribute}: {variant.variantAttributeTwo.value}
+                    <label htmlFor=" price"></label>
+                    <input placeholder="price" type="number" name="price" onChange={() => handleChange(variant.key, event)} />
+                    <label htmlFor="stock"></label>
+                    <input placeholder="stock" type="number" name="stock" onChange={() => handleChange(variant.key, event)} />
+                    <label htmlFor="imageURL"></label>
+                    <input placeholder="image url" type="text" name="imageURL" onChange={() => handleChange(variant.key, event)} />
+                    {/* <Button onClick={updateVariant(variant.key, variant.price, variant.stock, variant.imageURL)}> add</Button> */}
+                </div>
+            ))}
+
+            <br />
+            <br />
+            <br />
+            <br />
+
+            <Form onSubmit={()=> handleSubmit}>
+                <Button type="submit" variant="primary">submit product and variants</Button>
+            </Form>
+        </>
+    )
+}
+export default NewProductModal
+
+
+{/* {index} {element} <Form.Control placeholder="price" type="number" name="price" /> <Form.Control placeholder="stock" type="number" name="stock" /> */ }
